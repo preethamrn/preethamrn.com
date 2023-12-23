05a3a80d1072e325f03128424d16388ee4c77e95
preethamrn <preetham.in.1997@gmail.com>
2023-12-22 17:24:50 -0800

    Add error page

---
+++ preethamrn.com/error.vue
@@ -0,0 +1,31 @@
+<template>
+  <NuxtLayout>
+    <Container>
+      <h1 :v-if="error.statusCode === 404">404 - page not found</h1>
+      <p>Oops! That page returns an</p>
+      <NuxtImg
+        src="/errorsAllTheWayDown.png"
+        width="500"
+        quality="50"
+        alt="error text with a rainbow gradient and shadows creating a pop-out 3d effect"
+      />
+      <p v-if="newPath">
+        You might be looking for <NuxtLink :to="newPath">{{ newPath }}</NuxtLink>
+      </p>
+      <p v-else><NuxtLink to="/">Go home</NuxtLink></p>
+    </Container>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const route = useRoute();
+var newPath = "";
+
+let path = route.path;
+if (path.includes("/blog")) newPath = path.replace("/blog", "/posts");
+
+const { error } = defineProps({
+  error: Object,
+});
+console.error(error);
+</script>
