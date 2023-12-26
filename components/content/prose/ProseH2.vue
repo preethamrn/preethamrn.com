8ba896bce034d4350a46d34fb26986956ecd6dae
preethamrn <preetham.in.1997@gmail.com>
2023-12-25 17:27:43 -0800

    Fix CSS for heading links

---
+++ components/content/prose/ProseH2.vue
@@ -0,0 +1,15 @@
+<template>
+  <h2 :id="id">
+    <a v-if="generate" :href="`#${id}`" class="icon-link"> </a>
+    <slot />
+  </h2>
+</template>
+
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from "#imports";
+
+const props = defineProps<{ id?: string }>();
+
+const { headings } = useRuntimeConfig().public.mdc;
+const generate = computed(() => props.id && headings?.anchorLinks?.h1);
+</script>
---
+++ components/content/prose/ProseH3.vue
@@ -0,0 +1,15 @@
+<template>
+  <h3 :id="id">
+    <a v-if="generate" :href="`#${id}`" class="icon-link"> </a>
+    <slot />
+  </h3>
+</template>
+
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from "#imports";
+
+const props = defineProps<{ id?: string }>();
+
+const { headings } = useRuntimeConfig().public.mdc;
+const generate = computed(() => props.id && headings?.anchorLinks?.h1);
+</script>
