e2206b632310fd854145d80ba6f13243c8989191
preethamrn <preetham.in.1997@gmail.com>
2023-12-26 16:28:31 -0800

    Use Atom instead of RSS for feed

--- server/routes/feed.xml.ts
+++ server/routes/feed.xml.ts
@@ -1,39 +1,52 @@
-import RSS from "rss";
+import { Feed } from "feed";
 import { defineEventHandler } from "h3";
 import type { ParsedContent } from "@nuxt/content/dist/runtime/types";
 import { serverQueryContent } from "#content/server";
 
-// Source: https://github.com/rafaelmagalhaes/nuxt3-rss
+// Source: https://mokkapps.de/blog/create-an-rss-feed-with-nuxt-3-and-nuxt-content-v2
 export default defineEventHandler(async (event) => {
   // wrap everything in a try catch block
   try {
     // fetch contents
     const contentList = (await serverQueryContent(event).find()) as ParsedContent[];
+    const baseURL = "https://www.preethamrn.com";
 
-    // create new rss feed this will be our channel tag with website title and url
-    // TODO: consider using atom instead of RSS.
-    const feed = new RSS({
+    const feed = new Feed({
       title: "Preetham's Posts",
       description:
         "I write about productivity, software development, cubing, and various projects that I'm working on.",
-      site_url: "https://www.preethamrn.com", // link to your website/blog
-      feed_url: `https://www.preethamrn.com/rss`, // path to your rss feed
+      id: baseURL,
+      link: baseURL,
+      favicon: "http://www.preethamrn.com/favicon.png",
+      copyright: "All rights reserved 2023, Preetham Narayanareddy",
+      feedLinks: {
+        atom: baseURL + "/feed.xml",
+      },
+      author: {
+        name: "Preetham Narayanareddy",
+      },
     });
+
     contentList.sort((a, b) => (b.date > a.date ? 1 : -1));
     // loop over each posts
     for (const post of contentList) {
       if (post._dir !== "posts") continue;
       if (post.unlisted || post._draft) continue;
       // add item tag to our rss feed with correct data
-      feed.item({
-        title: post.title, // title from post to item title
-        url: `https://www.preethamrn.com/posts/${post.link}`, // full path to where our article is hosted
-        description: post.description, // post description
-        date: new Date(post.date), // date post was created
-        categories: post.tags, // list of tags
+      feed.addItem({
+        title: post.title,
+        id: `${baseURL}/posts/${post.link}`,
+        link: `${baseURL}/posts/${post.link}`,
+        description: post.description,
+        // TODO: consider adding post content here.
+        // NOTE: most feed readers get the image from the first large image in the content so it's a good idea to add here:
+        // see https://github.com/dword-design/nuxt-content-body-html
+        date: new Date(post.date),
+        categories: post.tags,
+        image: post.thumbnail ? `${baseURL}/posts/${post.thumbnail}` : `${baseURL}${post._path}/thumbnail.png`,
       });
     }
-    const feedString = feed.xml({ indent: true });
+    const feedString = feed.atom1();
 
     event.node.res.setHeader("content-type", "text/xml");
     event.node.res.end(feedString);
