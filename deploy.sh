ec564a783970cae582a9cc797a793d4cf69e27b0
preethamrn <preetham.in.1997@gmail.com>
2023-12-26 22:16:34 -0800

    Update deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/bash
 
 # abort on errors
 set -e
@@ -7,14 +7,26 @@ set -e
 bun run generate
 
 # navigate into the build output directory
-cd dist
+cd .output/public
 
 echo 'www.preethamrn.com' > CNAME
 
 git init
 git add -A
-git commit -m "deploy $1" 
+git commit -m "deploy $1"
 
-git push -f https://github.com/preethamrn/preethamrn.github.io master
+if [ "$1" = "staging" ]; then
+  # TODO: fix the staging environment "baseURL" since we can no longer access links through /
+  # It needs to be prefixed by /preethamrn-staging.com
+  git checkout -b gh-pages
+  git push -f https://github.com/preethamrn/preethamrn-staging.com gh-pages
+else
+  read -p "Deploying to production: Are you sure? " -n 1 -r
+  if [[ ! $REPLY =~ ^[Yy]$ ]]
+  then
+    [[ "$0" = "$BASH_SOURCE" ]] && echo && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+  fi
+  git push -f https://github.com/preethamrn/preethamrn.github.io master
+fi
 
 cd -
