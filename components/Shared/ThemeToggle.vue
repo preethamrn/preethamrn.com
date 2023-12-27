942f072e96c6e8fed815b08e9ceb980d9f34b732
preethamrn <preetham.in.1997@gmail.com>
2023-12-26 20:28:50 -0800

    Fix ThemeToggle not showing errors

--- components/Shared/ThemeToggle.vue
+++ components/Shared/ThemeToggle.vue
@@ -1,22 +1,6 @@
 <template>
   <div class="theme-toggle">
     <!-- e.preventDefault on mousedown to prevent double clicks from selecting text -->
-    <p v-if="$colorMode.value === 'dark'" @click="toggle('sepia')" @mousedown="(e) => e.preventDefault()">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="20"
-        height="20"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="feather feather-moon"
-      >
-        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
-      </svg>
-    </p>
     <p v-if="$colorMode.value === 'sepia'" @click="toggle('light')" @mousedown="(e) => e.preventDefault()">
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -37,7 +21,7 @@
         <line x1="14" y1="1" x2="14" y2="4"></line>
       </svg>
     </p>
-    <p v-if="$colorMode.value === 'light'" @click="toggle('dark')" @mousedown="(e) => e.preventDefault()">
+    <p v-else-if="$colorMode.value === 'light'" @click="toggle('dark')" @mousedown="(e) => e.preventDefault()">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="20"
@@ -61,11 +45,30 @@
         <line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>
       </svg>
     </p>
+    <p v-else @click="toggle('sepia')" @mousedown="(e) => e.preventDefault()">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-moon"
+      >
+        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+      </svg>
+    </p>
   </div>
 </template>
 
 <script setup>
 const colorMode = useColorMode();
+if (!colorMode.value || colorMode.value === "undefined") {
+  colorMode.preference = "system";
+}
 
 const toggle = (mode) => {
   colorMode.preference = mode;
